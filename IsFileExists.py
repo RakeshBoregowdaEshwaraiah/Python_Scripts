c42a960c93cf9e4a4089bf477c3318a7e3e87a19
Rakesh Boregowda Eshwaraiah <58733494+Rakesheshwaraiah@users.noreply.github.com>
2020-03-04 21:10:06 +0100

    Create IsFileExists.py

---
+++ IsFileExists.py
@@ -0,0 +1,21 @@
+import os
+
+# Path IN which we have to search file
+PATH = input("Enter the path where you want to check for file")   
+
+def searchFile(fileName):
+    ''' This function searches for the specified file name in the given PATH '''
+    for root, dirs, files in os.walk(PATH):
+        print('Looking in:',root)
+        for Files in files:
+            try:
+                found = Files.find(fileName)        # Returns -1 if NOT found else returns index
+                # print(found)
+                if found != -1:
+                    print(fileName, 'Found')
+                    break
+            except:
+                exit()
+
+if __name__ == '__main__':
+    searchFile('new_file.txt')
